73fa57341e02f107f6a1f4f6d73c208adb653e1a
webd <jhocelynespinoza@unitec.edu>
2024-12-10 18:58:21 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,109 @@
+// Dom7
+var $$ = Dom7;
+
+// Init App
+var app = new Framework7({
+  	id: 'com.app',
+  	root: '#app',
+	theme: 'md',
+	view: {
+        pushState: true
+    },
+	cache:false,
+	cacheDuration: 0,
+	modalTitle: 'App Framework7',
+	panel: {
+		swipe: true,
+	},
+  	routes: [
+		{
+		path: '/favoritos/',
+    	url: 'favoritos.html',
+    	name: 'favoritos',
+  		},
+		{
+		path: '/receta/',
+    	url: 'receta.html',
+    	name: 'vista',
+  		},
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/catalogo/',
+    	url: 'catalogo.html',
+    	name: 'catalogo',
+  		},
+		{
+		path: '/aboutus/',
+    	url: 'aboutus.html',
+    	name: 'aboutus',
+  		}
+		
+	],
+	dialog: {
+		title: 'Diseño Web App',
+		buttonOk: 'Aceptar',
+  	},
+	popup: {
+		closeOnEscape: true,
+		backdrop : false
+	},
+	sheet: {
+		closeOnEscape: true,
+	},
+	popover: {
+		closeOnEscape: true,
+	},
+	actions: {
+		closeOnEscape: true,
+	}
+});
+
+
+
+$$('#btnLogin').on('click', function (e) {
+	e.preventDefault();
+	
+	var $valid = $$('#form-login')[0].checkValidity();
+	if ($valid){
+        $$('#form-login').trigger('reset');	
+        app.loginScreen.close('#modal-login');
+		
+		app.dialog.alert('¡ Bienvenido a la APP !');
+    }
+	
+});
+
+
+
+$$(document).on('click', '#btnCheckout', function (e) {
+	e.preventDefault();
+
+	
+	app.dialog.confirm('¿Seguro desea finalizar su compra?', function () {
+		
+		//app.dialog.alert('Great!');
+		
+		var notification = app.notification.create({
+		  icon: '<i class="material-icons">check</i>',
+		  title: 'Order',
+		  titleRightText: '',
+		  subtitle: '',
+		  text: "Your order has been received.",
+		  closeTimeout: 3000,
+		});
+		notification.open();
+		
+	});
+	
+	
+	
+});
+
+
+
+
+
